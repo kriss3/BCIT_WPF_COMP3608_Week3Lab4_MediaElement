c754950bbb14584afeda39488a1404847786d7e4
kszczurowski <kszczurowski@absolute.com>
2017-11-17 10:44:16 -0800

    Initial commit. Working, compilable project

---
+++ MediaElementProject/ButtonStyle1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaElementProject
+{
+    /// <summary>
+    /// Interaction logic for ButtonStyle1.xaml
+    /// </summary>
+    public partial class ButtonStyle1 : UserControl
+    {
+        public ButtonStyle1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MediaElementProject/MediaElement.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MediaElementProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MediaElement : Window
+    {
+        public MediaElement()
+        {
+            InitializeComponent();
+            //playBtn.Click += new RoutedEventHandler(playBtn_Click);
+           // MyMediaElement.Play();
+        }
+
+        void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MyMediaElement.Play();
+        }
+
+        private void playBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MyMediaElement.Play();
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I was clicked","Message from Click Button",MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
