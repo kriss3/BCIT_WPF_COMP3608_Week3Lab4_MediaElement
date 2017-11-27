194e85713bf78156d82a8b8ea7275a88b8e58561
Krzysztof Szczurowski <kszczurowski@absolute.com>
2017-11-27 12:09:00 -0800

    Adding additonal avi video for toggle between current and new video. Adding additional button for toggling and functions for Pause, Stop and Start current video.

--- MediaElementProject/MediaElement.xaml.cs
+++ MediaElementProject/MediaElement.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System;
 using System.Windows.Input;
 
 namespace MediaElementProject
@@ -8,13 +9,45 @@ namespace MediaElementProject
     /// </summary>
     public partial class MediaElement : Window
     {
+        private bool _isPaused = true;
+
         public MediaElement()
         {
             InitializeComponent();
-            //playBtn.Click += new RoutedEventHandler(playBtn_Click);
-           MyMediaElement.Play();
+            playBtn.Click += PlayBtn_Click;
+            stopBtn.Click += StopBtn_Click;
+            stopBtn.PreviewMouseDown += StopBtn_PreviewMouseDown;
+            pauseBtn.Click += PauseBtn_Click;
         }
 
-       
+        private void PlayBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _isPaused = false;
+            MyMediaElement.Play();
+        }
+
+        private void StopBtn_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MyMediaElement.Position = new TimeSpan(0);
+        }
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MyMediaElement.Stop();
+        }
+
+        private void PauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isPaused == true)
+            {
+                _isPaused = false;
+                MyMediaElement.Play();
+            }
+            else
+            {
+                _isPaused = true;
+                MyMediaElement.Pause();
+            }
+            MyMediaElement.Pause();
+        }
     }
 }
