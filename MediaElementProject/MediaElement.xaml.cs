d62b220779a21ba477aa2a050dbc4fcb64ab782a
Krzysztof Szczurowski <kszczurowski@absolute.com>
2017-11-28 00:23:52 -0800

    Adding handling of toggle Button which switches between two videos/

--- MediaElementProject/MediaElement.xaml.cs
+++ MediaElementProject/MediaElement.xaml.cs
@@ -10,6 +10,7 @@ namespace MediaElementProject
     public partial class MediaElement : Window
     {
         private bool _isPaused = true;
+        private string _myVideo = "Resources/Running.avi";
 
         public MediaElement()
         {
@@ -18,6 +19,7 @@ namespace MediaElementProject
             stopBtn.Click += StopBtn_Click;
             stopBtn.PreviewMouseDown += StopBtn_PreviewMouseDown;
             pauseBtn.Click += PauseBtn_Click;
+            toggleBtn.Click += ToggleBtn_Click;
         }
 
         private void PlayBtn_Click(object sender, RoutedEventArgs e)
@@ -49,5 +51,30 @@ namespace MediaElementProject
             }
             MyMediaElement.Pause();
         }
+
+        private void ToggleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_myVideo == "Resources/Running.avi")
+            {
+                //set _vdieoVar to the next video              
+                _myVideo = "Resources/toy_plane_liftoff.avi";
+                // create a var called src of the type Uri              
+                // set the source of the Uri to the new video              
+                // inform it that the Uri is a Relative path             
+                Uri src = new Uri("Resources/toy_plane_liftoff.avi", UriKind.Relative);
+                // set MyMediaElement's Source                  
+                MyMediaElement.Source = src;
+                // Tell the MediaElement to play                 
+                MyMediaElement.Play();
+            }
+            else
+            {
+                _myVideo = "Resources/Running.avi";
+                Uri src = new Uri("Resources/Running.avi", UriKind.Relative);
+                MyMediaElement.Source = src;
+                MyMediaElement.Play();
+            }
+        }
+
     }
 }
